2ef7b5896759eaa736667d6ea9be392203857a8e
AlfanarAhmedAbdelhameed <143621075+AlfanarAhmedAbdelhameed@users.noreply.github.com>
2024-11-14 09:46:41 +0300

    Update aa.py

--- aa.py
+++ aa.py
@@ -48,7 +48,7 @@ class Pipeline:
         else:
             
             
-            return requests.post(self.API_URL, json={    "question": "Hey, how are you?",}).json().text
+            return requests.post(self.API_URL, json={    "question": "Hey, how are you?",}).text
             #return output
             #return self.API_URL
 
