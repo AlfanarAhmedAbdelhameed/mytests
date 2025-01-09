c35126cf9c124bf0679d9ddf483eaec63464e1e3
AlfanarAhmedAbdelhameed <143621075+AlfanarAhmedAbdelhameed@users.noreply.github.com>
2025-01-09 14:56:23 +0300

    Update aa.py

--- aa.py
+++ aa.py
@@ -44,12 +44,12 @@ class Pipeline:
     async def on_startup(self):
         # This function is called when the server is started.
         print(f"on_startup:{__name__}")
-        pass
+        #pass
 
     async def on_shutdown(self):
         # This function is called when the server is stopped.
         print(f"on_shutdown:{__name__}")
-        pass
+        #pass
 
     def pipe(
         self, user_message: str, model_id: str, messages: List[dict], body: dict
